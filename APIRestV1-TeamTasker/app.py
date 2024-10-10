62a04eecf3e57ca5fe638f591022b5ea2660fdf9
Matias Marzorati <147965244+valentinawerle@users.noreply.github.com>
2024-10-10 01:08:04 -0300

    creacion de register poco testeado

--- APIRestV1-TeamTasker/app.py
+++ APIRestV1-TeamTasker/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
-from Models.MySQLConnector import MySQLConnector
-import json
+from Models.SQLiteConnector import SQLiteConnector
 
 app = Flask(__name__)
 
@@ -12,7 +11,7 @@ def hello_world():
 # Ruta de login
 @app.route('/auth/login', methods=['POST'])
 def login():
-    db = MySQLConnector("config.json")
+    db = SQLiteConnector("config.json")
     db.connect()
 
     # Obtén los datos del cuerpo de la solicitud
@@ -25,7 +24,7 @@ def login():
         return jsonify({"error": "Faltan credenciales"}), 400
 
     # Consulta para validar el usuario
-    query = "SELECT * FROM users WHERE username = %s AND password = %s"
+    query = "SELECT * FROM users WHERE username = ? AND password = ?"
     result = db.fetch_data(query, (username, password))
 
     # Verifica si el usuario existe
@@ -34,12 +33,38 @@ def login():
     else:
         return jsonify({"error": "Credenciales incorrectas"}), 401
 
-if __name__ == '__main__':
-    db = MySQLConnector("config.json")
+# Ruta de registro
+@app.route('/auth/register', methods=['POST'])
+def register():
+    db = SQLiteConnector("config.json")
     db.connect()
 
-    # Ejecuta consultas o procedimientos almacenados
-    db.execute_query("CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(255))")
+    # Obtén los datos del cuerpo de la solicitud
+    user_data = request.get_json()
+    username = user_data.get('username')
+    password = user_data.get('password')
+
+    # Valida que los campos username y password estén presentes
+    if not username or not password:
+        return jsonify({"error": "Faltan datos"}), 400
+
+    # Verificar si el nombre de usuario ya existe
+    existing_user = db.fetch_data("SELECT * FROM users WHERE username = ?", (username,))
+    if existing_user:
+        return jsonify({"error": "El nombre de usuario ya existe"}), 409
+
+    # Insertar el nuevo usuario en la base de datos
+    insert_query = "INSERT INTO users (username, password) VALUES (?, ?)"
+    db.execute_query(insert_query, (username, password))
+
+    return jsonify({"message": "Usuario registrado exitosamente"}), 201
+
+if __name__ == '__main__':
+    db = SQLiteConnector("config.json")
+    db.connect()
+
+    # Crea la tabla de usuarios si no existe
+    db.execute_query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)")
 
     db.disconnect()
     app.run(debug=True)
