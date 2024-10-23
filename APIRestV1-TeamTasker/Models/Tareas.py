27b03d1f413b7c2e5d58c4973468e38c6496fa53
Diego-Escorche <diegoescorche11@gmail.com>
2024-10-23 09:52:13 -0300

    [Baja]: Corregidos errores con la indexacion y la logica

--- APIRestV1-TeamTasker/Models/Tareas.py
+++ APIRestV1-TeamTasker/Models/Tareas.py
@@ -54,40 +54,45 @@ class TareaGrupal(Tarea):
         self.idTareaGlobal = idTareaGlobal
         self.nombre = nombre
         
+        #Se insertan todos los datos en la base de datos
         query = """INSERT INTO tareagrupal (nombre, descripcion, idProyecto, admin, dateIn, dateEnd, completada, estado, etiqueta, prioridad)
         VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-
-        dbConnection.execute_query(query,(nombre, descripcion, idTareaGlobal, idUserLider, self.fecha_inicio, self.fecha_fin, 0, "",1,1))
+        dbConnection.execute_query(query,(nombre, descripcion, idTareaGlobal, idUserLider, self.fecha_inicio, self.fecha_fin, 0, "Verde",1,1))
         print("Consulta ejecutada correctamente")
 
         #Se le asignara a los integrantes esta tarea grupal.
         query = "get_id_grupo_proyecto"
         result = dbConnection.execute_stored_procedure(query, (idTareaGlobal, nombre))
+        
+        #Para confirmar que se lograra obtener el id de la tareas se realiza esta verificacion
         if result == None:
-            return "Error: Grupo no encontrado"
+            return jsonify({"message" : "Error: Grupo no encontrado"})
         try:
             self.idTarea = result[0][0]
         except Exception as e:
-            return f"Error: {e}"
+            return jsonify({"message" : f"Error: {e}"})
+        
         for integrante in integrantes:
             result = dbConnection.fetch_data("SELECT set_grupo_proyecto_usuario(%s, %s)", (self.idTarea, integrante))
             try:
                 if result[0][0] == 1:
                     return "OK"
             except:
-                return "Error: Al agregar"
+                return jsonify({"message" : "Error: Al agregar"})
         return "OK"
     
+    #Funcion para encontrar todas las tareas unitarias asociadas a una tarea grupal.
     def getTareasUnitarias(self, dbConnection: MySQLConnector):
         #Consigue todas las tareas unitarias de un grupo.
         query = """
-        SELECT idTareaUnitaria
+        SELECT *
         FROM tareaunitaria tu
         JOIN tareagrupal tg ON tu.grupo = tg.idGrupo
         WHERE tg.idGrupo = %s """
         tareasUnitarias = dbConnection.fetch_data(query,(self.idTarea,))
         return tareasUnitarias
     
+    #Funcion para conseguir todos los atributos de una tarea grupal usando su nombre y el id de la tarea global
     def asignarTareaGrupal(self, dbConnection: MySQLConnector):
         data_tareagrupal = dbConnection.execute_stored_procedure("get_id_grupo_proyecto", (self.idTareaGlobal, self.nombre))
         self.idTarea = data_tareagrupal[0][0]
@@ -99,49 +104,47 @@ class TareaGrupal(Tarea):
         self.etiqueta = data_tareagrupal[0][10]
         self.idAdminGrupo = data_tareagrupal[0][4]
     
-    # Función para calcular el progreso real vs el progreso esperado
+    #Función para calcular el progreso real vs el progreso esperado
     def calcularProgreso(self, dbConnection: MySQLConnector):
         #Obtener todas las tareas unitarias asociadas
-        tareasUnitarias = self.getTareasUnitarias(dbConnection)
+        tareasUnitarias = dbConnection.execute_stored_procedure("get_grupo_tareas_proyecto", (self.idTarea,))
         if not tareasUnitarias:
             return jsonify({'message': 'No hay tareas unitarias asociadas a esta tarea grupal'}), 404
-        
-        #Sumar los progresos de todas las tareas unitarias
-        progreso_total = sum([tarea['progreso'] for tarea in tareasUnitarias])
-        
-        #Dividir el progreso total entre el número de tareas unitarias para obtener el progreso promedio
-        total_tareas = len(tareasUnitarias)
-        progreso_promedio = progreso_total / total_tareas if total_tareas > 0 else 0
-        
-        # Calcular los días de duración de la tarea grupal
-        duracion_total_dias = (self.fecha_fin - self.fecha_inicio).days
-        if duracion_total_dias <= 0:
-            return jsonify({'message': 'Duración inválida para la tarea grupal'}), 400
+        try:
+            #Calcular el promedio del progreso de las tareas unitarias
+            progreso_total = sum([tarea[6] for tarea in tareasUnitarias])
+            total_tareas = len(tareasUnitarias)
+            self.progreso = progreso_total / total_tareas if total_tareas > 0 else 0
+            
+            # Calcular los días de duración de la tarea grupal
+            duracion_total_dias = (self.fecha_fin - self.fecha_inicio).days
+            if duracion_total_dias <= 0:
+                return jsonify({'message': 'Duración inválida para la tarea grupal'}), 400
 
-        # Calcular los días transcurridos desde la fecha de inicio hasta hoy
-        dias_transcurridos = (datetime.now() - self.fecha_inicio).days
-
-        # Si la fecha actual supera la fecha de finalización, se espera un progreso del 100%
-        if datetime.now() > self.fecha_fin:
-            progreso_esperado = 100
-        else:
-            # Calcular el progreso esperado en función de los días transcurridos
-            # Asegurarse de no exceder la duración total
-            dias_transcurridos = min(dias_transcurridos, duracion_total_dias)
-        
-            progreso_diario_esperado = 100 / duracion_total_dias
-            progreso_esperado = progreso_diario_esperado * dias_transcurridos
-        
-        # 5. Comparar el progreso actual con el esperado
-        if progreso_promedio >= progreso_esperado:
-            estado = "Verde"
-        elif progreso_promedio >= (progreso_esperado * 0.75):
-            estado = "Amarillo"
-        else:
-            estado = "Rojo"
+            # Si la fecha actual supera la fecha de finalización, se espera un progreso del 100%
+            if datetime.now() > self.fecha_fin:
+                progreso_esperado = 100
+            else:
+                # Calcular los días transcurridos desde la fecha de inicio hasta hoy
+                dias_transcurridos = (datetime.now() - self.fecha_inicio).days
+                
+                # Calcular el progreso esperado en función de los días transcurridos
+                dias_transcurridos = min(dias_transcurridos, duracion_total_dias)
+                progreso_diario_esperado = 100 / duracion_total_dias
+                progreso_esperado = progreso_diario_esperado * dias_transcurridos
+            
+            #Comparar el progreso actual con el esperado
+            if self.progreso >= progreso_esperado:
+                self.estado = "Verde"
+            elif self.progreso >= (progreso_esperado * 0.75):
+                self.estado = "Amarillo"
+            else:
+                self.estado = "Rojo"
+        except:
+            return jsonify({"message" : "Error al calcular el progreso."})
         
         # Retornar el estado del progreso actual comparado con el esperado
         return jsonify({
-            'progreso_promedio': progreso_promedio,
-            'estado': estado
+            'progreso': self.progreso,
+            'estado': self.estado
         }), 200
--- APIRestV1-TeamTasker/app.py
+++ APIRestV1-TeamTasker/app.py
@@ -7,7 +7,7 @@ from Models.Tareas import *
 
 app = Flask(__name__)
 
-# Endpoint para que el Administrador de Proyecto cree una tarea grupal, usando el id del proyecto en la ruta
+#Endpoint para que el Administrador de Proyecto cree una tarea grupal, usando el id del proyecto en la ruta
 @app.route('/<int:idTareaGlobal>/crear_tarea_grupal', methods=['POST'])
 def crearTareaGrupal(idTareaGlobal):
     #Primero chequear que la solicitud sea un post
@@ -32,7 +32,7 @@ def crearTareaGrupal(idTareaGlobal):
         
         #Verificar que no exista una tareaGrupal con el mismo nombre en el mismo proyecto.
         check = existeTareaGrupal(db, idTareaGlobal, nombre)
-        if not  check == "OK":
+        if not check == "OK":
             return check, 401
         
         #Tambien se realizaran verificaciones de que los integrantes y el admin del nuevo grupo pertenezcan al proyecto.
@@ -59,7 +59,7 @@ def crearTareaGrupal(idTareaGlobal):
         
     return jsonify({'message': 'Metodo no valido'}), 405
 
-# Endpoint para que el Administrador de Proyecto elimine una tarea grupal
+#Endpoint para que el Administrador de Proyecto elimine una tarea grupal
 @app.route('/<int:idTareaGrupal>/eliminar_tarea_grupal', methods=['DELETE'])
 def eliminarTareaGrupal(idTareaGrupal):
     if request.method == 'DELETE':
@@ -85,54 +85,95 @@ def eliminarTareaGrupal(idTareaGrupal):
         tareaGrupal = TareaGrupal()
         tareaGrupal.idTarea = idTareaGrupal  # Asignar el id de la tarea grupal que se desea eliminar
         tareasUnitarias = tareaGrupal.getTareasUnitarias(db)
+        
+        try:
+            # Verificar si hay tareas unitarias asociadas a la tarea grupal
+            if tareasUnitarias:
+                # Verificar si las tareas unitarias han comenzado (esto se sabra por la etiqueta)
+                tareasEmpezadas = [tarea for tarea in tareasUnitarias if not tarea[9] == "Inactiva"]
 
-        # Verificar si hay tareas unitarias asociadas a la tarea grupal
-        if tareasUnitarias:
-            # Verificar si las tareas unitarias han comenzado (suponiendo que el progreso indica si han comenzado)
-            tareasEmpezadas = [tarea for tarea in tareasUnitarias if tarea['etiqueta'] == "Inactiva"]
+                if tareasEmpezadas:
+                    if accion == 'asignar':
+                        # Obtener los nuevos grupos desde el JSON
+                        nuevosGrupos = data.get('nuevosGrupos', [])
 
-            if tareasEmpezadas:
-                if accion == 'asignar':
-                    # Lógica para asignar las tareas unitarias a otros grupos
-                    nuevosGrupos = data.get('nuevosGrupos', [])
-                    if not nuevosGrupos or len(nuevosGrupos) != len(tareasUnitarias):
-                        return jsonify({'message': 'No se han asignado todas las tareas a nuevos grupos'}), 400
+                        # Validar que se han proporcionado suficientes grupos para todas las tareas unitarias
+                        if not nuevosGrupos or len(nuevosGrupos) != len(tareasUnitarias):
+                            return jsonify({'message': 'No se han asignado todas las tareas a nuevos grupos'}), 400
 
-                    # Asignar las tareas a los nuevos grupos
-                    for tarea, nuevoGrupo in zip(tareasUnitarias, nuevosGrupos):
-                        query = "UPDATE tareaunitaria SET grupo = %s WHERE idTareaUnitaria = %s"
-                        db.execute_query(query, (nuevoGrupo, tarea['idTareaUnitaria']))
+                        # Verificar que todos los nuevos grupos existen en la base de datos
+                        query = "SELECT idGrupo FROM grupos WHERE idGrupo IN (%s)" % ','.join(['%s'] * len(nuevosGrupos))
+                        grupos_existentes = db.fetch_data(query, tuple(nuevosGrupos))
 
-                elif accion == 'continuar':
-                    # El usuario decide continuar sin asignar las tareas, se elimina la tarea grupal
-                    pass
+                        if len(grupos_existentes) != len(nuevosGrupos):
+                            return jsonify({'message': 'Algunos de los grupos proporcionados no existen'}), 400
+
+                        # Asignar cada tarea unitaria a un nuevo grupo existente
+                        for tarea, nuevoGrupo in zip(tareasUnitarias, nuevosGrupos):
+                            query = "UPDATE tareaunitaria SET grupo = %s WHERE idTareaUnitaria = %s"
+                            db.execute_query(query, (nuevoGrupo, tarea[0]))
+
+                    elif accion == 'continuar':
+                        # El usuario decide continuar sin asignar las tareas, se elimina la tarea grupal
+                        query = "DELETE FROM tareaunitaria WHERE grupo = %s"
+                        db.execute_query(query, (idTareaGrupal,))
+                    else:
+                        # En caso de que no se seleccione ninguna acción válida
+                        return jsonify({'message': 'Acción no válida'}), 400
                 else:
-                    # En caso de que no se seleccione ninguna acción válida
-                    return jsonify({'message': 'Acción no válida'}), 400
+                    query = "DELETE FROM tareaunitaria WHERE grupo = %s"
+                    db.execute_query(query, (idTareaGrupal,))
+        except:
+            pass
 
         # Proceder con la eliminación de la tarea grupal
         query = """DELETE FROM tareagrupal WHERE idGrupo = %s"""
-        result = db.execute_query(query, (idTareaGrupal,))
-
-        if result == "OK":
-            return jsonify({'message': 'Tarea grupal eliminada correctamente'}), 200
-        else:
-            return jsonify({'message': 'Error al eliminar la tarea grupal'}), 500
+        db.execute_query(query, (idTareaGrupal,))
+        query = """SELECT idGrupo FROM tareagrupal WHERE idGrupo = %s"""
+        result = db.fetch_data(query, (idTareaGrupal,))
+        if not result:
+            return jsonify({"message" : "Tarea Grupal eliminada satisfactoriamente"})
+        return jsonify({'message': 'Error al eliminar la tarea grupal'}), 500
 
     return jsonify({'message': 'Método no válido'}), 405
 
+#Endpoint que le dara toda la informacion de las tareas unitarias asociadas
+@app.route('/<int:idTareaGrupal>/get_tareas_unitarias', methods=['GET'])
+def getTareasUnitarias(idTareaGrupal):
+    if request.method == 'GET':
+        tareaGrupal = TareaGrupal()
+        tareaGrupal.idTarea = idTareaGrupal
+        tareasUnitarias = tareaGrupal.getTareasUnitarias(db)
+        if tareasUnitarias:
+            return jsonify({"message" : tareasUnitarias}), 200
+        else:
+            return jsonify({"message" : "Este grupo no tiene tareas grupales"}), 200
+    return jsonify({'message': 'Método no válido'}), 405
+
+#Endpoint que conseguira todas las tareas grupales asociadas a la tareaGlobal
+@app.route('/<int:idTareaGlobal>/get_tareas_grupales', methods=['GET'])
+def getTareasGrupales(idTareaGlobal):
+    if request.method == 'GET':
+        query = "SELECT * FROM tareagrupal WHERE idProyecto = %s"
+        result = db.fetch_data(query, (idTareaGlobal, ))
+        if result:
+            return jsonify({"message" : result}), 200
+    return jsonify({'message': 'Método no válido'}), 405
 
 #Funcion para verificar que el id pertenece al del admin del proyecto
-def verificarAdminProyecto(db: MySQLConnector, id: int, idTareaGlobal: int):
+def verificarAdminProyecto(id: int, idTareaGlobal: int):
     query = """SELECT admin_id FROM tareaglobal WHERE idProyecto = %s"""
     result = db.fetch_data(query, (idTareaGlobal,))
-    if not (result[0][0] == id):
+    try:
+        if not (result[0][0] == id):
+            return jsonify({'message': "No es el admin del proyecto"})
+        else:
+            return "OK"
+    except:
         return jsonify({'message': "No es el admin del proyecto"})
-    else:
-        return "OK"
-    
+
 #Funcion para verificar que todos los integrantes pertenezcan al proyecto.
-def verificarIntegrantes(db: MySQLConnector, integrantes, idTareaGlobal: int, idAdminGrupo, idAdminProyecto):
+def verificarIntegrantes(integrantes, idTareaGlobal: int, idAdminGrupo, idAdminProyecto):
     result = db.execute_stored_procedure("get_usuarios_proyecto", (idTareaGlobal,))
     if not result == None:
         for usuario in result:
@@ -142,16 +183,17 @@ def verificarIntegrantes(db: MySQLConnector, integrantes, idTareaGlobal: int, id
     return "OK"
 
 #Verificar que exista una tarea grupal con el mismo nombre
-def existeTareaGrupal(db: MySQLConnector, idTareaGlobal: int, nombre):
+def existeTareaGrupal(idTareaGlobal: int, nombre):
     query = """SELECT nombre FROM tareagrupal WHERE idProyecto = %s"""
     result = db.fetch_data(query, (idTareaGlobal,))
     try:
         for resultado in result:
             if resultado[0] == nombre:
-                return "Error: Ya existe una tarea grupal con ese nombre."
+                return jsonify({"message" : "Error: Ya existe una tarea grupal con ese nombre."})
     except:
         return "OK"
     return "OK"
+
 db = MySQLConnector('APIRestV1-TeamTasker/config.json')
 db.connect()
 if __name__ == '__main__':
