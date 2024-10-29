8b686c08f297d64f371c1e7b3d6b70a2e9a156c8
Matias Marzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2024-10-29 18:42:52 -0300

    cambiamos el puerto y el cors

--- api2/src/main.ts
+++ api2/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Permitir todas las IPs y dominios
+  });
+
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000);
+  await app.listen(13000);
 }
 bootstrap();
