10c9a6d69b304d5492f52664eed5d2c2a6611796
Matias Marzorati <146393923+MatiasMarzorati@users.noreply.github.com>
2024-11-27 11:02:58 -0300

    Update tasks.module.ts

--- api2/src/tasks/tasks.module.ts
+++ api2/src/tasks/tasks.module.ts
@@ -5,6 +5,7 @@ import { TasksController } from './tasks.controller';
 import { Task, TaskSchema } from './schemas/task.schema'; // Importa el esquema de Task
 import { GroupalTask, GroupalTaskSchema } from '../groupal-tasks/schemas/groupal-task.schema';
 import { GlobalTask, GlobalTaskSchema } from '../global-task/schemas/global-task.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { GlobalTask, GlobalTaskSchema } from '../global-task/schemas/global-task
       { name: GroupalTask.name, schema: GroupalTaskSchema },  // Registrar GroupalTask
       { name: GlobalTask.name, schema: GlobalTaskSchema },  // Registrar GlobalTask
     ]),
+    UserModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
